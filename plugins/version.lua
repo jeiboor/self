89e96ac65b643488d0317b26aa7f0f0c0e2f90a2
MR_saf <jeiboor@users.noreply.github.com>
2016-06-27 02:38:01 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,9 +1,9 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v4 Edited by Security King
-pv : @Xx_SECURITY_xX
-ch : @Nod32TM"
+    return "Self-Bot v4 Edited by Soheyl Dalton
+pv : @DaltonOfficial
+ch : @DaltonTeam"
 end 
 return {
   patterns = {
